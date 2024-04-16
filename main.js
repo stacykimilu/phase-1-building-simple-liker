3ac417226980c53bc2985dce9a01b4ee094fbc8e
Stacy <stacykimilu@gmail.com>
2024-04-16 09:11:17 +0300

    building simple liker

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const likeButtons = document.querySelectorAll(".like");
 
-
-
+  // Add event listener to each like button
+  likeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const glyph = button.querySelector(".like-glyph");
+      mimicServerCall()
+        .then(() => {
+          if (glyph.textContent === EMPTY_HEART) {
+            glyph.textContent = FULL_HEART;
+            glyph.classList.add("activated-heart");
+          } else {
+            glyph.textContent = EMPTY_HEART;
+            glyph.classList.remove("activated-heart");
+          }
+        })
+        .catch((error) => {
+          const errorModal = document.getElementById("modal");
+          const modalMessage = document.getElementById("modal-message");
+          modalMessage.textContent = error;
+          errorModal.classList.remove("hidden");
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
